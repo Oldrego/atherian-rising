ac7e32878a0baca420ddf4250bd0e1c544f09b09
Glitch (hello-webpage) <none>
2019-05-06 21:09:07 +0000

    🏊🏼📚 Checkpoint ./script.js:4291818/2197 ./index.html:4291818/787 ./style.css:4291818/3218

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-
-
-function getTimeRemaining(endtime){
+function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor( (t/1000) % 60 );
-  var minutes = Math.floor( (t/1000/60) % 60 );
-  var hours = Math.floor( (t/(1000*60*60)) % 24 );
-  var days = Math.floor( t/(1000*60*60*24) );
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
   return {
     'total': t,
     'days': days,
@@ -15,16 +13,29 @@ function getTimeRemaining(endtime){
   };
 }
 
-function initializeClock(id, endtime){
+function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
-  var timeinterval = setInterval(function(){
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
     var t = getTimeRemaining(endtime);
-    clock.innerHTML = 'days: ' + t.days + ' ' +
-                      'hours: '+ t.hours + ' ' +
-                      'minutes: ' + t.minutes + ' ' +
-                      'seconds: ' + t.seconds;
-    if(t.total<=0){
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
       clearInterval(timeinterval);
     }
-  },1000);
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
 }
+
+var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
